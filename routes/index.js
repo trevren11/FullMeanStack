6dcc325ba089fe22cc96d20f859302469a1ef296
Trevor Renshaw <trevren11@gmail.com>
2017-03-24 04:41:48 +0000

    Set process id for server

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Comment = mongoose.model('Comment');
 
+process.title = "nodeComment3009"; // Set the process id
+
 router.get('/comments', function(req, res, next) {
   Comment.find(function(err, comments){
     if(err){ return next(err); }
